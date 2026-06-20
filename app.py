37aa757492f660301447a1677d13f8865174256e
Vijayashree-08 <vijayashreek420@gmail.com>
2026-06-20 12:37:38 +0530

    Connect Flask with HTML pages

--- app.py
+++ app.py
@@ -4,18 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>VIDHYA</h1>
-    <h3>AI Powered Student Learning Platform</h3>
-    <p>Welcome to our Hackathon Project</p>
-    """
+    return render_template("index.html")
+
 
 @app.route("/about")
 def about():
-    return """
-    <h2>About VIDHYA</h2>
-    <p>VIDHYA helps students learn smarter.</p>
-    """
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
